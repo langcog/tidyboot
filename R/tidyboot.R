41f110180eda418fbc9fc38dbc6abb0adbef0e58
Mika Braginsky <mika.br@gmail.com>
2017-09-20 14:01:49 -0400

    tidyboot binds samples into one data frame instead of mapping, tidyboot_mean doesn't support na.rm

--- R/tidyboot.R
+++ R/tidyboot.R
@@ -1,4 +1,7 @@
+if (getRversion() >= "2.15.1") utils::globalVariables("stat")
+
 #' @importFrom dplyr "%>%"
+#' @importFrom dplyr n
 NULL
 
 #' Non-parametric bootstrap for numeric vector data
@@ -72,21 +75,17 @@ tidyboot.logical <- function(data,
 #' Computes arbitrary bootstrap statistics on univariate data.
 #'
 #' @param data A data frame.
-#' @param summary_function A function to be computed over each set of samples as
-#'   a data frame, or a string that is the name of the function to be computed
-#'   over each set of samples as a single column of a data frame indicated by
-#'   \code{column} (defaults to \code{mean}).
 #' @param column A column of \code{data} to bootstrap over (if not supplied,
 #'   summary_function and statistic_function must operate over the appropriate
 #'   data frame).
-#' @param summary_groups A vector of strings that are column names of
-#'   \code{data} indicating how it should be grouped before applying
-#'   \code{summary_function}.
-#' @param statistics_functions A named list of functions to be computed over the
-#'   data frame of summary values from all samples.
-#' @param statistics_groups A vector of strings that are column names of
-#'   \code{data} indicating how it should be grouped before applying
-#'   \code{statistics_functions} (defaults to \code{summary_groups}).
+#' @param summary_function A function to be computed over each set of samples as
+#'   a data frame, or a function to be computed over each set of samples as a
+#'   single column of a data frame indicated by \code{column} (defaults to
+#'   \code{mean}).
+#' @param statistics_functions A function to be computed over each set of
+#'   samples as a data frame, or a named list of functions to be computed over
+#'   each set of samples as a single column of a data frame indicated by
+#'   \code{column}.
 #' @param nboot The number of bootstrap samples to take (defaults to
 #'   \code{1000}).
 #' @param ... Other arguments passed from generic.
@@ -96,29 +95,22 @@ tidyboot.logical <- function(data,
 #' require(dplyr)
 #' gauss1 <- data_frame(value = rnorm(1000, mean = 0, sd = 1), condition = 1)
 #' gauss2 <- data_frame(value = rnorm(1000, mean = 2, sd = 3), condition = 2)
-#' tidyboot(data = bind_rows(gauss1, gauss2),
-#'          summary_function = mean, column = value, summary_groups = "condition",
-#'          statistics_functions = list("ci_lower" = ci_lower, "mean" = mean, "ci_upper" = ci_upper))
-#' tidyboot(data = bind_rows(gauss1, gauss2),
-#'          summary_function = function(df) df %>% summarise(mean = mean(value)),
-#'          summary_groups = "condition",
-#'          statistics_functions = function(df) df %>%
-#'            summarise_at(vars(mean), funs(ci_upper, mean, ci_lower)),
-#'          statistics_groups = "condition",
-#'          nboot = 100)
+#' df <- bind_rows(gauss1, gauss2)
+#' df %>% group_by(condition) %>%
+#'   tidyboot(column = value, summary_function = mean,
+#'            statistics_functions = list("ci_lower" = ci_lower, "mean" = mean, "ci_upper" = ci_upper))
+#' df %>% group_by(condition) %>%
+#'   tidyboot(summary_function = function(x) x %>% summarise(mean = mean(value)),
+#'            statistics_functions = function(x) x %>%
+#'            summarise_at(vars(mean), funs(ci_upper, mean, ci_lower)))
 #' @export
 tidyboot.data.frame <- function(data,
-                                summary_function = mean,
                                 column = NULL,
-                                summary_groups = NULL,
+                                summary_function = mean,
                                 statistics_functions,
-                                statistics_groups = summary_groups,
                                 nboot = 1000, ...) {
 
-  assertthat::assert_that(all(statistics_groups %in% summary_groups))
-
-  # TODO: do something smart if no groups
-  original_groups <- dplyr::groups(data)
+  data_groups <- dplyr::groups(data)
 
   column <- rlang::enquo(column)
 
@@ -145,120 +137,91 @@ tidyboot.data.frame <- function(data,
     }
   }
 
-  if(!is.null(summary_groups))
-    summary_groups <- chr_to_quo(summary_groups)
+  empirical_summary <- data %>%
+    call_summary_function()
 
-  if (!is.null(summary_groups)) {
-    data <- data %>% dplyr::group_by(!!!summary_groups)
+  names(empirical_summary) <- names(empirical_summary) %>%
+    purrr::modify_if(~!(.x %in% groups(data)), ~paste0("empirical_", .x)) %>%
+    unlist()
+
+  n_summary <- data %>%
+    dplyr::summarise(n = n())
+
+  samples <- data %>%
+    modelr::bootstrap(n = nboot) %>%
+    dplyr::mutate(strap = purrr::map(strap, dplyr::as_data_frame)) %>%
+    tidyr::unnest()
+
+  if (!rlang::is_null(data_groups)) {
+    samples <- samples %>% dplyr::group_by(!!!data_groups, .id)
+  } else {
+    samples <- samples %>% dplyr::group_by(.id)
   }
 
-  samples <- data %>% modelr::bootstrap(n = nboot)
+  sample_vals <- samples %>%
+    call_summary_function()
 
-  summarise_sample <- function(df) {
-    if (!is.null(summary_groups)) {
-      df <- df %>% dplyr::group_by(!!!summary_groups)
-    }
-    df %>% call_summary_function()
-  }
-
-  sample_vals <- samples$strap %>%
-    purrr::map_df(~.x$data %>% summarise_sample)
-
-  if (is.null(statistics_groups) & !is.null(original_groups))
-    sample_vals <- sample_vals %>% dplyr::group_by(!!!original_groups)
-
-  if (!is.null(statistics_groups)) {
-    statistics_groups <- chr_to_quo(statistics_groups)
-    sample_vals <- sample_vals %>% dplyr::group_by(!!!statistics_groups)
+  if (!is.null(data_groups)) {
+    sample_vals <- sample_vals %>% dplyr::group_by(!!!data_groups)
   }
 
   booted_vals <- call_statistics_functions(sample_vals)
 
-  return(booted_vals)
+  if (nrow(empirical_summary) > 1) {
+    n_summary %>%
+      dplyr::left_join(empirical_summary,
+                       by = as.character(dplyr::groups(data))) %>%
+      dplyr::left_join(booted_vals,
+                       by = as.character(dplyr::groups(data)))
+  } else {
+    n_summary %>%
+      dplyr::bind_cols(empirical_summary) %>%
+      dplyr::bind_cols(booted_vals)
+  }
+
 }
 
 
 #' Nonparemetric Bootstrap and Empirical central tendency for data frames
 #' Designed to make standard use of \code{tidyboot.data.frame} easier
 #'
-#' Computes arbitrary bootstrap statistics on univariate data.
-#' NOTE: Both empirical functions and bootstrapping functions will be computed
-#'  over the grouping variables currently specified for the data frame.
+#' Computes arbitrary bootstrap statistics on univariate data. NOTE: Both
+#' empirical functions and bootstrapping functions will be computed over the
+#' grouping variables currently specified for the data frame.
 #'
 #' @param data A data frame.
-#' @param column A string indicating the column of \code{data} to bootstrap
-#' @param na.rm A logical indicating whether NAs should be dropped before
-#'  bootstrapping (defaults to \code{NULL})
-#' @param empirical_function a string indicating the function to compute and
-#'  bootstrap over (defaults to \code{mean})
-#' @param statistics_functions A vector of strings that are names of functions
-#'  to be bootstrapped (defaults to \code{c("ci_lower", "ci_upper")})
-#' @param nboot The number of bootstrap samples to take (defaults to \code{1000}).
-#'
+#' @param column A column of \code{data} to bootstrap over.
+#' @param nboot The number of bootstrap samples to take (defaults to
+#'   \code{1000}).
 #' @examples
 #' ## Mean and 95% confidence interval for 1000 samples from two different normal distributions
 #' require(dplyr)
-#' gauss1 <- data.frame(value = rnorm(1000, mean = 0, sd = 1), condition = 1)
-#' gauss2 <- data.frame(value = rnorm(1000, mean = 2, sd = 3), condition = 2)
-#' df <- bind_rows(gauss1, gauss2) %>%
-#'  group_by(condition)
-#'  tidyboot_default(data = df, column = value, nboot = 10)
+#' gauss1 <- data_frame(value = rnorm(1000, mean = 0, sd = 1), condition = 1)
+#' gauss2 <- data_frame(value = rnorm(1000, mean = 2, sd = 3), condition = 2)
+#' df <- bind_rows(gauss1, gauss2)
+#' df %>%
+#'  group_by(condition) %>%
+#'  tidyboot_mean(column = value)
 #' @export
-tidyboot_default <- function(data, column, na.rm = NULL,
-                             empirical_function = mean,
-                             statistics_functions = list("ci_lower" = ci_lower,
-                                                         "ci_upper" = ci_upper),
-                             nboot = 1000) {
+tidyboot_mean <- function(data, column, nboot = 1000) {
 
-  #assertthat::assert_that(typeof(empirical_function) == "character")
   column <- rlang::enquo(column)
 
-  # if (!is.null(na.rm)) {
-  #   empirical_dots <- list(lazyeval::interp(~fun(arg, na.rm = na.rm),
-  #                                           fun = as.name(empirical_function),
-  #                                           arg = as.name(column)))
-  #
-  #   statistics_funs <- sapply(
-  #     statistics_functions,
-  #     function(x) lazyeval::interp(~fun(., na.rm = na.rm), fun = as.name(x))
-  #   )
-  #
-  #   statistics_formulas <- function(df)
-  #     # df %>% dplyr::summarise_at(dplyr::vars(summary),
-  #     #                            dplyr::funs_(statistics_funs))
-  #     df %>% dplyr::summarise_at(dplyr::vars(!!column),
-  #                                dplyr::funs(!!!statistics_funs))
-  #
-  #
-  # } else {
-  #   empirical_dots <- list(lazyeval::interp(~fun(arg),
-  #                                           fun = as.name(empirical_function),
-  #                                           arg = as.name(column)))
-  #
-  #   statistics_formulas <- statistics_functions
-  # }
+  summary_function <- function(df) {
+    df %>% dplyr::summarise(stat = mean(!!column))
+  }
 
-  # call_empirical_function <- function(df) {
-  #   dplyr::summarise_(df, .dots = stats::setNames(empirical_dots, "summary"))
-  # }
+  statistics_functions <- function(df) {
+    df %>% dplyr::summarise(ci_lower = ci_lower(stat),
+                            mean = mean(stat),
+                            ci_upper = ci_upper(stat))
+  }
 
-  booted_data <- tidyboot(data, summary_function = empirical_function,
-                          column = !!column,
-                          statistics_functions = statistics_functions,
-                          nboot = nboot)
+  tidyboot(data,
+           summary_function = summary_function,
+           statistics_functions = statistics_functions,
+           nboot = nboot)
 
-  mean_df <- data %>%
-    dplyr::summarise_at(dplyr::vars(!!column), dplyr::funs(!!empirical_function))
-
-  if(nrow(mean_df) > 1)
-    dplyr::left_join(mean_df, booted_data,
-                     by = as.character(dplyr::groups(data)))
-  else
-    dplyr::bind_cols(mean_df, booted_data)
-
-  # call_empirical_function(data) %>%
-  #   dplyr::left_join(booted_data) %>%
-  #   dplyr::rename_(.dots = stats::setNames("summary", empirical_function))
 }
 
 #' Non-parametric bootstrap with multiple sample statistics
